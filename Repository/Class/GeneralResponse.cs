c86681def52b63d7e8fe71b95f0eb301cb5df086
Heidari@rahkarpouya.local <Heidari@rahkarpouya.local>
2023-09-01 20:00:16 +0430

    update add dal and Repository

---
+++ Repository/Class/GeneralResponse.cs
@@ -0,0 +1,34 @@
+﻿namespace Repository.Class
+{
+    public class GeneralResponse
+    {
+        public GeneralResponse()
+        {
+
+        }
+        public GeneralResponse(bool IsSuccess = false, string Message = "", string ErrorMessage = "")
+        {
+            this.IsSuccess = IsSuccess;
+            this.Message = Message;
+            this.ErrorMessage = ErrorMessage;
+        }
+        public bool IsSuccess { get; set; }
+        public string Message { get; set; }
+        public string ErrorMessage { get; set; }
+    }
+
+    public class GeneralResponse<T>
+    {
+        public GeneralResponse(T obj, bool IsSuccess = false, string Message = "", string ErrorMessage = "")
+        {
+            this.IsSuccess = IsSuccess;
+            this.Message = Message;
+            this.ErrorMessage = ErrorMessage;
+            this.obj = obj;
+        }
+        public bool IsSuccess { get; set; }
+        public string Message { get; set; }
+        public string ErrorMessage { get; set; }
+        public T obj { get; set; }
+    }
+}
